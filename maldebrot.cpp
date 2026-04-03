f2f5a95a9b5abb4624fb849319aee6f94f9722a3
Zinawe <kostkost2007@yandex.ru>
2026-04-03 17:44:42 +0300

    circle

--- maldebrot.cpp
+++ maldebrot.cpp
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include "raylib.h"
 
+const int HIEGHT = 600;
+const int WIDTH  = 800;
+
 int main()
 {
-    InitWindow(800, 600, "MALDEBROT");
+    InitWindow(WIDTH, HIEGHT, "MALDEBROT");
 
     int N_max = 255;
     float r2_max = 2;
     bool is_in_set = false;
+    float R = 100.f;
 
     while (!WindowShouldClose()) 
     {
@@ -21,34 +25,13 @@ int main()
         for (int i_y = 0; i_y < 600; ++i_y)
         {   
 
-            float ci = -1.2 + (float)i_y / 600 * 2.4;
-
             for (int i_x = 0; i_x < 800; ++i_x)
             {
-                float cr = -2.0 + (float)i_x / 800 * 3.0;  
+                float X = i_x - WIDTH  / 2;
+                float Y = i_y - HIEGHT / 2;
 
-                float X = 0;
-                float Y = 0;
-                
-                struct Color color = WHITE;
-
-                for (int N = 0; N < N_max; N++)
-                {
-                    float x2 = X * X;
-                    float y2 = Y * Y;
-                    float xy = X * Y;
-
-                    float r2 = x2 + y2;
-
-                    if (r2 >= r2_max) 
-                        color = BLACK;
-                        break;
-
-                    X = x2 - y2 + ci;
-                    Y = xy + xy + cr;
-                }
-
-                DrawPixel(i_x, i_y, color);
+                if (X * X + Y * Y <= R * R)
+                DrawPixel(i_x, i_y, WHITE);
             }
         }
 
