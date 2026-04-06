3bfef8105d831793ee57ae943fc56b818ef7be76
Zinawe <kostkost2007@yandex.ru>
2026-04-06 11:45:37 +0300

    version with funcs

--- maldebrot.cpp
+++ maldebrot.cpp
@@ -8,65 +8,151 @@
 const int HEIGHT = 600;
 const int WIDTH  = 800;
 
+void mm128_set_ps(float vec[8], float x);
+void mm128_add_ps(float dst[8], float src[8]);
+void mm128_sub_ps(float dst[8], float src[8]);
+void mm128_mul_ps(float dst[8], float src[8]);
+void mm128_cpy_ps(float dst[8], float src[8]);
+void mm128_cmp_ps(float a[8], float b[8], int cmp[8]);
+
+void mm128_set_ps(float vec[8], float x)
+{
+    for (int i = 0; i < 8; i++) vec[i] = x;
+}
+
+void mm128_add_ps(float dst[8], float src[8])
+{
+    for (int i = 0; i < 8; i++) dst[i] += src[i];
+}
+
+void mm128_sub_ps(float dst[8], float src[8])
+{
+    for (int i = 0; i < 8; i++) dst[i] -= src[i];
+}
+
+void mm128_mul_ps(float dst[8], float src[8])
+{
+    for (int i = 0; i < 8; i++) dst[i] *= src[i];
+}
+
+void mm128_cpy_ps(float dst[8], float src[8])
+{
+    for (int i = 0; i < 8; i++) dst[i] = src[i];
+}
+
+void mm128_cmp_ps(float a[8], float b[8], int cmp[8])
+{
+    for (int i = 0; i < 8; i++) a[i] <= b[i] ? cmp[i] = 1 : cmp[i] = 0;
+}
+
 int main()
 {
     InitWindow(WIDTH, HEIGHT, "MALDEBROT");
 
-    int   N_max    = 256;
-    float r2_max   = 4;
-    float x_offset = 0;
-    float y_offset = 0;
-    float zoom     = 1;
-
-    int aaaa = 0;
+    int   N_max     = 256;
+    float x_offset  = 0;
+    float y_offset  = 0;
+    float dx        = 1.f / (float)WIDTH;
+    float dy        = 1.f / (float)HEIGHT;
+    float zoom      = 2.5;
+    float _0123[8]  = {0.f, 1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f};
+    float r2_max[8] = {4.f, 4.f, 4.f, 4.f, 4.f, 4.f, 4.f, 4.f};
 
     while (!WindowShouldClose()) 
     {
         if (IsKeyDown(KEY_ESCAPE))       break;
-        if (IsKeyDown(KEY_LEFT))         x_offset -= 10 * zoom;
-        if (IsKeyDown(KEY_RIGHT))        x_offset += 10 * zoom;
-        if (IsKeyDown(KEY_UP))           y_offset -= 10 * zoom;
-        if (IsKeyDown(KEY_DOWN))         y_offset += 10 * zoom;
-        if (IsKeyDown(KEY_PAGE_UP))      zoom     /= 1.1;
-        if (IsKeyDown(KEY_PAGE_DOWN))    zoom     *= 1.1;
+
+        if (IsKeyDown(KEY_LEFT))         x_offset -= 10 * dx * zoom;
+        if (IsKeyDown(KEY_RIGHT))        x_offset += 10 * dx * zoom;
+        if (IsKeyDown(KEY_UP))           y_offset -= 10 * dy * zoom;
+        if (IsKeyDown(KEY_DOWN))         y_offset += 10 * dy * zoom;
+        if (IsKeyDown(KEY_PAGE_UP))      zoom     *=  1.1;
+        if (IsKeyDown(KEY_PAGE_DOWN))    zoom     /=  1.1;
 
         BeginDrawing();
         ClearBackground(BLACK);
 
         clock_t start_time = clock();
 
-        for (int i_y = 0; i_y < HEIGHT; ++i_y)
+        for (int y_i = 0; y_i < HEIGHT; y_i++)
         {   
-            for (int i_x = 0; i_x < WIDTH; ++i_x)
+            float coeff[8] = {};
+            mm128_set_ps(coeff, dx * zoom);
+            mm128_mul_ps(_0123, coeff);
+
+            for (int x_i = 0; x_i < WIDTH; x_i += 8)
             {
+                float x0[8] = {};
+                float y0[8] = {};
 
-                float x_0 = -0.5 + ((((float)i_x -  WIDTH / 2)  * zoom) + x_offset) / WIDTH  * 2.5;
-                float y_0 =        ((((float)i_y - HEIGHT / 2)  * zoom) + y_offset) / HEIGHT * 2.5;  
+                float x_0 = (((float)x_i - WIDTH  / 2) * dx) * zoom + x_offset;
+                float y_0 = (((float)y_i - HEIGHT / 2) * dy) * zoom + y_offset; 
 
-                float X = 0;
-                float Y = 0;
+               // printf("x_0: %g    y_0: %g\n", dx, y_0);
+
+                mm128_set_ps(x0, x_0);
+                mm128_set_ps(y0, y_0);
+
+                //for (int i = 0; i < 8; ++i) printf("%f %f %f %f\n", x0[0], x0[1], x0[2], x0[3]);
+
+                mm128_add_ps(x0, _0123);
+
+                float X[8] = {};
+                float Y[8] = {};
+
+                mm128_cpy_ps(X, x0);
+                mm128_cpy_ps(Y, y0);
                 
-                struct Color color = BLACK;
+                int cmp[8] = {};
+                int N[8]   = {};
 
-                for (int N = 0; N < N_max; N++)
+                for (int N_i = 0; N_i < N_max; N_i++)
                 {
-                    float x2 = X * X;
-                    float y2 = Y * Y;
-                    float xy = X * Y;
+                    float x2[8] = {};
+                    float y2[8] = {};
+                    float xy[8] = {};
 
-                    float r2 = x2 + y2;
+                    mm128_cpy_ps(x2, X);
+                    mm128_cpy_ps(y2, Y);
+                    mm128_cpy_ps(xy, X);
 
-                    if (r2 >= r2_max)
-                    {
-                        color = WHITE;
+                    mm128_mul_ps(x2, x2);
+                    mm128_mul_ps(y2, y2);
+                    mm128_mul_ps(xy, Y);
+
+                    float r2[8] = {};
+                    mm128_cpy_ps(r2, x2);
+                    mm128_add_ps(r2, y2);
+
+                    int cmp[8] = {};
+                    mm128_cmp_ps(r2, r2_max, cmp);
+
+                    int mask = 0;
+                    for (int i = 0 ; i < 8; ++i) mask += cmp[i] << i;
+                    if (!mask)
                         break;
-                    }
 
-                    X = x2 - y2 + x_0;
-                    Y = xy + xy + y_0;
+                    mm128_cpy_ps(X, x2);
+                    mm128_sub_ps(X, y2);
+                    mm128_add_ps(X, x0);
+
+                    mm128_cpy_ps(Y, xy);
+                    mm128_add_ps(Y, xy);
+                    mm128_add_ps(Y, y0);
+
+                    for (int i = 0; i < 8; ++i) N[i] += cmp[i];
                 }
 
-                DrawPixel(i_x, i_y, color);
+                //for (int i = 0; i < 8; ++i) printf("%d %d %d %d\n", N[0], N[1], N[2], N[3]);
+
+                DrawPixel(x_i,     y_i, (Color){255 - N[0], 255 - N[0], 255 - N[0], 255});
+                DrawPixel(x_i + 1, y_i, (Color){255 - N[1], 255 - N[1], 255 - N[1], 255});
+                DrawPixel(x_i + 2, y_i, (Color){255 - N[2], 255 - N[2], 255 - N[2], 255});
+                DrawPixel(x_i + 3, y_i, (Color){255 - N[3], 255 - N[3], 255 - N[3], 255});
+                DrawPixel(x_i + 4, y_i, (Color){255 - N[4], 255 - N[4], 255 - N[4], 255});
+                DrawPixel(x_i + 5, y_i, (Color){255 - N[5], 255 - N[5], 255 - N[5], 255});
+                DrawPixel(x_i + 6, y_i, (Color){255 - N[6], 255 - N[6], 255 - N[6], 255});
+                DrawPixel(x_i + 7, y_i, (Color){255 - N[7], 255 - N[7], 255 - N[7], 255});
             }
         }
 
